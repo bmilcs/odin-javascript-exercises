6dbebd2428aca942e1d6798ad7ada9c25bc674ef
bmilcs <bmilcs@yahoo.com>
2022-07-25 15:02:02 -0400

    Complete lesson #1 & #2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,7 @@
-const repeatString = function() {
-
+const repeatString = function (string, repeats) {
+  let finalString = "";
+  for (let i = 0; i < repeats; i++) finalString = finalString + string;
+  return finalString;
 };
 
 // Do not edit below this line
