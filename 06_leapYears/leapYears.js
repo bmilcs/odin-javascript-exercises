c9380a8b01c733b9b60390643c9e573b8ba59383
bmilcs <bmilcs@yahoo.com>
2022-07-25 18:26:11 -0400

    Add odin solution to problem #6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -8,6 +8,9 @@ const leapYears = function (year) {
   } else return false;
 };
 
+// Odin solution
+// return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+
 console.log(leapYears(2000)); // is a leap year: returns true
 console.log(leapYears(1985)); // is not a leap year: returns false
 
