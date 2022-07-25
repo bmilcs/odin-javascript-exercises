5b11d7bfac36270e338b843a1e349d83c7a429de
bmilcs <bmilcs@yahoo.com>
2022-07-25 15:33:12 -0400

    Complete lesson #3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  // create reversedString variable
+  let reversedString = "";
 
+  // loop the string's length
+  for (let i = 0; i < string.length; i++) {
+    // grab the substr of current index and add it to reversedString
+    reversedString = reversedString + string.substr(-i - 1, 1);
+  }
+
+  // return final result
+  return reversedString;
 };
 
 // Do not edit below this line
