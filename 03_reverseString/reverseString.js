1de73d22e5a022827c19d22bae4f25eb26930250
bmilcs <bmilcs@yahoo.com>
2022-07-25 15:45:07 -0400

    Add odin solution to problem #3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -7,10 +7,14 @@ const reverseString = function (string) {
     // grab the substr of current index and add it to reversedString
     reversedString = reversedString + string.substr(-i - 1, 1);
   }
-
   // return final result
   return reversedString;
 };
+
+// ODIN'S SOLUTION
+// const reverseString = function (string) {
+//   return string.split("").reverse().join("");
+// };
 
 // Do not edit below this line
 module.exports = reverseString;
