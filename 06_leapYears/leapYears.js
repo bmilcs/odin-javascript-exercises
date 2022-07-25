018542e9ad580016435479c9a4913eb8dd14dafc
bmilcs <bmilcs@yahoo.com>
2022-07-25 18:23:15 -0400

    Complete lesson 5

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  // leap years are divisible by 4
+  if (year % 4 === 0) {
+    // years divisible by 100 are NOT leap years UNLESS divisible by 400
+    if (year % 100 === 0 && year % 400 === 0) return true;
+    else if (year % 100 === 0) return false;
+    else return true;
+  } else return false;
 };
+
+console.log(leapYears(2000)); // is a leap year: returns true
+console.log(leapYears(1985)); // is not a leap year: returns false
 
 // Do not edit below this line
 module.exports = leapYears;
