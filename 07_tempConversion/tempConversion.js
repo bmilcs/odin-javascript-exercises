a3b98efd6d85b783bac245f8c0262761b2798186
bmilcs <bmilcs@yahoo.com>
2022-07-26 14:57:02 -0400

    Complete lesson 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (fTemp) {
+  let cTemp = ((fTemp - 32) * (5 / 9)).toFixed(1);
+  return +cTemp;
 };
 
-const ctof = function() {
-
+const ctof = function (cTemp) {
+  let fTemp = (cTemp * (9 / 5) + 32).toFixed(1);
+  return +fTemp;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
