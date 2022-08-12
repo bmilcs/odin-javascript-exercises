ae83ab0e56b32d8210f51d7c6b0d3349fc5c0645
bmilcs <bmilcs@yahoo.com>
2022-08-12 15:45:33 -0400

    Complete lesson 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (args) {
+  return args.reduce((previous, current) => previous + current, 0);
 };
 
-const multiply = function() {
+const multiply = function (args) {
+  // return args.reduce((previous, current) => previous * current, 1);
 
+  // odin
+  return array.length
+    ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
+    : 0;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0) return 1;
+  // let num = n;
+  // for (i = 1; i < n; i++) num *= i;
+  // return num;
+
+  // odin: recursive factorial
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +40,13 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
+
+let array = [1, 2, 3, 4, 5];
+let summy = sum(array);
+let multy = multiply(array);
+console.log(`Array: ${[array]}`);
+console.log(`Sum: ${summy}`);
+console.log(`Multiply: ${multy}`);
+console.log(`Factorial 5: ${factorial(5)}`);
